eebcb6256250f1862c1cd54678d4e1a47572466e
jinishkanpara <jkanpara1996@gmail.com>
2019-05-26 19:16:39 -0700

    itspythonupdated

--- jinish.sh
+++ jinish.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-echo "jinish kanpara"
+
 csvcut -c 15,17 flightdelays.csv | csvgrep -c 2 -m SFO | head -n 4 | csvlook > first3sfo.csv
+echo "jinish kanpara"
--- jkpython.sh
+++ jkpython.sh
@@ -2,3 +2,4 @@
 import pandas as pd
 jk = pd.read_csv("flightdelays.csv")
 print(jk[jk[["ArrDelay","Origin"]]["Origin"]  == "SFO"][["ArrDelay","Origin"]].head(3))
+print("jinish kanpara.")
